d3b6d27ae3f3cafd8170c1337adbcb19c0e6e53f
NPC2048 <35749025+NPC2048@users.noreply.github.com>
2021-07-20 16:32:41 +0800

    刷新时无菜单问题

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,30 @@
 </template>
 
 <script>
-// import tabbar from "@/layout/tabbar";
-import {useStore} from "vuex";
-import netKit from "@/utils/netKit";
+import {useStore} from 'vuex';
+import netKit from '@/utils/netKit';
+import {DEFAULT_MENU_LIST} from './utils/global';
 
 export default {
   name: 'App',
-  components: {
-    // tabbar
-  },
   created() {
     const store = useStore();
     // 页面加载时读取 storage 里的状态信息
-
+    netKit.get('/system/shutdown').then(r => {
+      console.log('r:', r);
+    })
+    console.log(netKit.axios.defaults.baseURL)
     const storageStore = localStorage.getItem('store');
     if (storageStore) {
       store.replaceState(Object.assign({}, store.state, JSON.parse(storageStore)));
       // 拉取菜单
       netKit.get('/menu/menus').then(menuList => {
         // 更新到 store
-        store.commit('setMenuList', menuList);
+        if (menuList.length) {
+          store.commit('setMenuList', menuList);
+        } else {
+          store.commit('setMenuList', DEFAULT_MENU_LIST);
+        }
       })
     }
     // 页面刷新时存储 store 到 sessionStorage
